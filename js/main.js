0082abbe43d8465ea1079ea54ede3c9576ae1d99
mifist <avanturka1992@gmail.com>
2018-07-18 10:39:55 +0300

    keywords functionality

--- js/main.js
+++ js/main.js
@@ -1,41 +1,40 @@
 var $admin_sidebar = $('.admin-sidebar-wrap'), // only search once
-	$page_container = $('.job-wrapper > .main-content');
-	$page_header = $('header > .wrapper');
+    $page_container = $('.job-wrapper > .main-content');
+$page_header = $('header > .wrapper');
 
 $(window).bind('load resize orientationChange', function () {
-	
-	var pos =  $admin_sidebar.position();
-	var	width = (($(window).width() - $page_container.width())/2);
-	var sidebar = $admin_sidebar.width();
 
-	if (width < sidebar) {
-		$page_container.css('margin-left', sidebar);
-		$page_header.css('margin-left', sidebar);
-	}
-	
+    var pos = $admin_sidebar.position();
+    var width = (($(window).width() - $page_container.width()) / 2);
+    var sidebar = $admin_sidebar.width();
+
+    if (width < sidebar) {
+        $page_container.css('margin-left', sidebar);
+        $page_header.css('margin-left', sidebar);
+    }
+
 });
 
 /* Admin Search page -> filter pill */
 
 
-$(document).on('click', '.filter.admin .filter-row > .close', function(e) {
-	e.preventDefault();
-	$(this).parent().remove();
+$(document).on('click', '.filter.admin .filter-row > .close', function (e) {
+    e.preventDefault();
+    $(this).parent().remove();
 });
 
 $('.filter.admin .stop-btn').on('click', function (e) {
-	e.preventDefault();
-	$.each($('.filter.admin .filter-rows .filter-row:not(.hidden)'), function () {
+    e.preventDefault();
+    $.each($('.filter.admin .filter-rows .filter-row:not(.hidden)'), function () {
         $(this).remove();
-	});
+    });
 });
 $('.filter.admin .add-btn').on('click', function (e) {
     e.preventDefault();
-	var $add_row = $('.filter.admin').find('.filter-rows .filter-row.hidden').clone().removeClass('hidden');
-	$('.filter-rows').append($add_row);
-	
-});
+    var $add_row = $('.filter.admin').find('.filter-rows .filter-row.hidden').clone().removeClass('hidden');
+    $('.filter-rows').append($add_row);
 
+});
 
 
 $.each($('.search-module .sel-wrap-default select'), function (index) {
@@ -94,7 +93,7 @@ $('select:not(.rating)').on('select2:opening', function (e) {
     $(this).closest('.sel-wrap').removeClass('is-focused');
 });
 
-$('.save').click(function() {
+$('.save').click(function () {
     var $this = $(this);
     if ($this.hasClass('add_save')) {
         $this.removeClass('add_save');
@@ -105,10 +104,10 @@ $('.save').click(function() {
     }
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     //Check to see if the window is top if not then display button
-    $(window).scroll(function(){
+    $(window).scroll(function () {
         if ($(this).scrollTop() > 400) {
             $('.scrollToTop').fadeIn();
         } else {
@@ -117,30 +116,29 @@ $(document).ready(function(){
     });
 
     //Click event to scroll to top
-    $('.scrollToTop').on('show.bs.dropdown',function(){
-        $('html, body').animate({scrollTop : 0},800);
+    $('.scrollToTop').on('show.bs.dropdown', function () {
+        $('html, body').animate({scrollTop: 0}, 800);
         return false;
     });
-    
+
     $('.admin-button').on('click', function (e) {
         e.preventDefault();
         $('.admin-sidebar-wrap').addClass('show');
         $(this).fadeOut();
     });
-    
-	$('.admin-sidebar-wrap > .close').on('click', function (e) {
-		e.preventDefault();
-		$('.admin-sidebar-wrap').removeClass('show');
-		$('.admin-button').fadeIn();
-	});
-    
-    
+
+    $('.admin-sidebar-wrap > .close').on('click', function (e) {
+        e.preventDefault();
+        $('.admin-sidebar-wrap').removeClass('show');
+        $('.admin-button').fadeIn();
+    });
+
+
 });
-$('select.dropdown').dropdown('set selected', ['php','UI']);
+$('select.dropdown').dropdown('set selected', ['php', 'UI']);
 
 
-
-$('.checkdown').click(function() {
+$('.checkdown').click(function () {
     var $this = $(this);
     $this.removeClass('open');
     if ($this.hasClass('active')) {
@@ -154,3 +152,301 @@ $('.checkdown').click(function() {
 
 
 //------------------
+
+//------------------
+/* search for niche*/
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('search-input');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("search-list");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("label")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+
+function getSelectionHtml() {
+    var html = "";
+    if (typeof window.getSelection != "undefined") {
+        var sel = window.getSelection();
+        if (sel.rangeCount) {
+            var container = document.createElement("div");
+            for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+                container.appendChild(sel.getRangeAt(i).cloneContents());
+            }
+            html = container.innerText;
+            console.log(html);
+        }
+    } else if (typeof document.selection != "undefined") {
+        if (document.selection.type == "Text") {
+            html = document.selection.createRange().htmlText;
+        }
+    }
+    return html;
+
+}
+
+/* Read  json file  */
+$.getJSON('db.json', function (data) {
+    var job = data.job;
+
+    $(document).ready(addToSearchList());
+
+
+    $.each(job, function (k) {
+        $("#check" + k).change(function () {
+            if (this.checked) {
+                job[k].jobSelected = true;
+                addToViewList();
+                CheckList();
+                addKeyTabMenu();
+                addKeyTabContent();
+
+            } else {
+                job[k].jobSelected = false;
+                job[k].jobCheck = false;
+                addToViewList();
+                CheckList();
+                addKeyTabMenu();
+                addKeyTabContent();
+                console.log(job);
+            }
+        });
+    });
+
+    function checkSelect() {
+        $.each(job, function (k) {
+            $("#check" + k).change(function () {
+                if (this.checked) {
+                    job[k].jobSelected = true;
+                    addToViewList();
+                    CheckList();
+                    addKeyTabMenu();
+                    addKeyTabContent();
+
+                } else {
+                    job[k].jobSelected = false;
+                    job[k].jobCheck = false;
+                    addToViewList();
+                    CheckList();
+                    addKeyTabMenu();
+                    addKeyTabContent();
+                    console.log(job);
+                }
+            });
+        });
+        addToViewList();
+        CheckList();
+        addKeyTabMenu();
+        addKeyTabContent();
+    }
+
+    function addToSearchList() {
+        $("#search-list").html("");
+        for (var i in job) {
+            var selectOption = job[i].jobSelected;
+            if (selectOption == false) {
+                $("#search-list").append('<li><input id="check' + i + '" type="checkbox" value="' + job[i].jobNiche + '"><label for="check' + i + '">' + job[i].jobNiche + '</label></li>');
+            } else {
+                $("#search-list").append('<li><input id="check' + i + '" type="checkbox" value="' + job[i].jobNiche + '" checked><label for="check' + i + '">' + job[i].jobNiche + '</label></li>');
+            }
+        }
+    }
+
+    /*update view list */
+    function addToViewList() {
+        $(".views-list").html("");
+        for (var t  in  job) {
+            if (job[t].jobSelected == true) {
+                $(".views-list").append('<input id="ch' + t + '" type="checkbox" value="' + job[t].jobNiche + '"  checked><label for="ch' + t + '">' + job[t].jobNiche + '</label>');
+                job[t].jobCheck = true;
+            }
+        }
+    }
+
+    /*update tab menu */
+    function addKeyTabMenu() {
+        $("#keyword-list").html("");
+        for (var t  in  job) {
+            if (job[t].jobCheck == true) {
+                $("#keyword-list").append('<li><a data-toggle="tab" href="#home' + t + '">' + job[t].jobNiche + '</li>');
+            }
+        }
+    }
+
+
+    /*update tab content */
+    function addKeyTabContent() {
+        addKeyTabMenu();
+
+        var jobKey = [];
+        $(".tab-content ").html("");
+        for (var t  in  job) {
+            if (job[t].jobCheck == true) {
+                jobKey = job[t].kaywords;
+
+                $(".tab-content").append('<div id="home' + t + '" class="tab-pane  fade in "><ul></ul></div>');
+                for (var m in jobKey) {
+                    $("#home" + t + ' ul').append('<li class="li-' + m + '">-' + jobKey[m].keyText + '<button class="editKey"><i class="fa fa-edit"></i></button><button class="removeKey" ><i class="fa fa-times"></i></button></li>');
+                    var instance = new Mark(document.querySelector("div.job-description"));
+                    instance.mark(jobKey[m].keyText, {
+                        "element": "span",
+                        "className": "select-key",
+                        accuracy: "exactly",
+                        separateWordSearch: true,
+                    });
+                }
+            }
+        }
+        $('#keyword-list li:first a').tab('show');
+
+    }
+
+
+    /*checked error  value  input niche */
+    function addToJson() {
+        var a = $("form.niche").serializeArray();
+        /*checked error  value  input niche */
+        var p = [];
+        $.each(a, function () {
+            for (var i = 0; i < job.length; i++) {
+                if (job[i].jobNiche == this.value) {
+                    p = true;
+                    console.log(p);
+                }
+            }
+        });
+        $.each(a, function () {
+            if (this.value == '') {
+                console.log("Pleas entry value");
+                alert("Pleas entry value")
+            } else if (p == true) {
+                console.log("Pleas entry another value");
+                alert("Pleas entry another value");
+            } else {
+                var jobID = job.length + 1;
+                var jobNiche = this.value;
+                var jobSelected = true;
+                var jobCheck = true;
+                var kaywords = [];
+                mas = {
+                    'jobID': jobID,
+                    'jobNiche': jobNiche,
+                    'jobSelected': jobSelected,
+                    'jobCheck': jobCheck,
+                    'kaywords': kaywords
+                };
+                job.push(mas);
+                addToSearchList();
+                checkSelect();
+            }
+        });
+        console.log(job);
+    }
+
+
+    /*------------------------------------------------------*/
+
+    function addToKay() {
+        var a = $("form.keywoard").serializeArray();
+        var y = [];
+        $.each(a, function () {
+            for (var i = 0; i < job.length; i++) {
+                if (job[i].jobCheck == true) {
+                    var key = job[i].kaywords;
+                    for (var j = 0; j < key.length; j++) {
+                        if (key[j].keyText == this.value) {
+                            y = true;
+                            console.log(y);
+                        }
+                    }
+                }
+            }
+        });
+
+        $.each(a, function () {
+            if (this.value == '') {
+                console.log("Pleas entry value");
+                alert("Pleas entry value")
+            } else if (y == true) {
+                console.log("Pleas entry another value");
+                alert("Pleas entry another value");
+            } else {
+                var jobKey = [];
+                for (var i = 0; i < job.length; i++) {
+                    if (job[i].jobCheck == true) {
+                        jobKey = job[i].kaywords;
+                        console.log(job.length);
+                        var p = {
+                            keyID: jobKey.length + 1,
+                            keyText: this.value
+                        };
+                        jobKey.push(p);
+                    }
+                }
+                addKeyTabContent();
+            }
+            console.log(job);
+        });
+    }
+
+    function CheckList() {
+        $.each(job, function (k) {
+            $("#ch" + k).change(function () {
+                if (this.checked) {
+                    job[k].jobCheck = true;
+                    console.log(job);
+                    addKeyTabMenu();
+                    addKeyTabContent();
+
+                }
+                else {
+                    job[k].jobCheck = false;
+                    console.log(job);
+                    addKeyTabMenu();
+                    addKeyTabContent();
+
+                }
+            });
+        });
+    }
+
+    /* create  new niche*/
+    $(".push-value").click(function () {
+        addToJson();
+        console.log(job);
+    });
+
+    document.getElementById('description').addEventListener("mouseup", function () {
+
+        var slection = getSelectionHtml();
+        if (slection == '') {
+
+        } else {
+            $('#key').val(slection);
+            addToKay();
+            $('#key').val('');
+        }
+
+    });
+    $(".push-key").click(function () {
+        addToKay();
+
+    });
+
+
+});
+
+
+
+
